e06c91afea4d6657c9090bff92cd80d96119b952
Kourosh Khansary <kkhansary@gmail.com>
2017-04-29 12:21:15 +0430

    Base of interactions.

--- Application.vb
+++ Application.vb
@@ -1,7 +1,34 @@
 ﻿Public Module Application
 
     Public Sub Main()
-
+        Run()
+        WaitHandle.WaitOne()
     End Sub
 
+    Private Async Sub Run()
+        Do
+            Console.Write("-> ")
+            Dim Command = Console.ReadLine()
+
+            Select Case Command
+                Case "Help"
+                    Help()
+                Case "Quit"
+                    Exit Do
+                Case Else
+                    Console.WriteLine("Invalid command. Write Help for see commands.")
+            End Select
+
+            Console.WriteLine()
+        Loop
+
+        WaitHandle.Set()
+    End Sub
+
+    Private Sub Help()
+        Console.WriteLine("Commands: ")
+        Console.WriteLine("Quit")
+    End Sub
+
+    Private ReadOnly WaitHandle As Threading.EventWaitHandle = New Threading.EventWaitHandle(False, Threading.EventResetMode.ManualReset)
 End Module
